0c8881d64e2c6074dd1b94ef51c53dfa49be5f34
FidelisArumala <60404832+FidelisArumala@users.noreply.github.com>
2020-03-24 00:31:59 +0100

    Add files via upload

---
+++ Javascript/index.js
@@ -0,0 +1,10 @@
+var name='Fidelis Arumala';
+console.log(name);
+var courses= ['HTML','CSS','JAVASCRIPT']
+console.log(courses);
+var i;
+{ for (i=1; i<=200; i++)
+if (i % 2!=0)
+console.log(i);
+}
+
